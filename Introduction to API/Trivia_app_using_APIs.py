91cb9bc5d40288d49a207cfb5d4400f906048e9c
Bryce-Chinniah407 <bryce.chinniah@gmail.com>
2025-11-26 18:27:43 +0400

    Trivia app using public api

--- Introduction to API/Trivia_app_using_APIs.py
+++ Introduction to API/Trivia_app_using_APIs.py
@@ -5,9 +5,9 @@ import html
 EDUCATION_CATEGORY_ID = 9
 API_URL = f"https://opentdb.com/api.php?amount=10&category={EDUCATION_CATEGORY_ID}&type=multiple"
 def get_education_questions():
-    responce = requests.get(API_URL)
-    if responce.status_code == 200:
-        data = responce.json()
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        data = response.json()
         if data['response_code'] == 0 and data['results']:
             return data['results']
         return None
@@ -20,14 +20,14 @@ def run_quiz():
     score = 0
     print("Welcome to the Education Quiz!\n")
     for i, q in enumerate(questions, 1):
-        questions = html.unescape(q['question'])
+        question = html.unescape(q['question'])
         correct = html.unescape(q['correct_answer'])
         incorrects = [html.unescape(a) for a in q['incorrect_answers']]
         options = incorrects +[correct]
         random.shuffle(options)
-        print(f"Question {i}:\n{questions}")
-        for idx, options in enumerate(options, 1):
-            print(f"    {idx}. {options}")
+        print(f"Question {i}: {question}")
+        for idx, option in enumerate(options, 1):
+            print(f"    {idx}. {option}")
 
         while True:
             try:
@@ -44,7 +44,7 @@ def run_quiz():
         else:
             print(f"❌ Incorrect!!! Correct answer: {correct}\n")
 
-    print(f"Final Scaore: {score}/{len(questions)}")
+    print(f"Final Score: {score}/{len(questions)}")
     print(f"Percentage: {score/len(questions)*100:.1f}%")
 if __name__ == "__main__":
     run_quiz()
